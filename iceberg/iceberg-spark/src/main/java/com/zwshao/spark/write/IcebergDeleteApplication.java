c975f8dec949cb233bf8cb57845e0df05a914605
xiaoshao <zengwei19861020@163.com>
2023-08-01 15:35:38 +0800

    iceberg

--- iceberg/iceberg-spark/src/main/java/com/zwshao/spark/write/IcebergDeleteApplication.java
+++ iceberg/iceberg-spark/src/main/java/com/zwshao/spark/write/IcebergDeleteApplication.java
@@ -1,7 +1,20 @@
 package com.zwshao.spark.write;
 
+import com.zwshao.spark.read.spark.utils.SparkUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 public class IcebergDeleteApplication {
     public static void main(String[] args) {
+        String deleteDataPath = args[0];
+        String icebergLocation = args[1];
 
+        SparkSession sparkSession = SparkUtils.createLocalSession(new SparkConf(), "iceberg_delete_application");
+
+        Dataset<Row> deleteData = sparkSession.read().format("csv").schema(SparkUtils.createSchema()).load(deleteDataPath);
+
+        deleteData.write().format("iceberg").save(icebergLocation);
     }
 }
--- iceberg/iceberg-spark/src/main/java/com/zwshao/spark/write/IcebergWriteApplication.java
+++ iceberg/iceberg-spark/src/main/java/com/zwshao/spark/write/IcebergWriteApplication.java
@@ -1,8 +1,21 @@
 package com.zwshao.spark.write;
 
+import com.zwshao.spark.read.spark.utils.SparkUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
 public class IcebergWriteApplication {
 
     public static void main(String[] args) {
+        String originDataPath = args[0];
+        String icebergLocation = args[1];
 
+        SparkSession sparkSession = SparkUtils.createLocalSession(new SparkConf(), "iceberg_origin_application");
+
+        Dataset<Row> originData = sparkSession.read().format("csv").schema(SparkUtils.createSchema()).load(originDataPath);
+
+       originData.writeTo("")
     }
 }
