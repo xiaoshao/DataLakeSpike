b032433c20b96d329d3d84f9f4ef2f989b8865ab
xiaoshao <zengwei19861020@163.com>
2023-08-14 17:19:06 +0800

    hudi sdk

--- hudi/hudi-sdk/src/main/java/com/zw/sdk/utils/SDKConst.java
+++ hudi/hudi-sdk/src/main/java/com/zw/sdk/utils/SDKConst.java
@@ -1,9 +1,65 @@
 package com.zw.sdk.utils;
 
+import org.apache.avro.Schema;
+import org.apache.commons.io.FileUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hudi.common.fs.FSUtils;
+import org.apache.hudi.common.model.HoodieAvroPayload;
+import org.apache.hudi.common.model.HoodieTableType;
+import org.apache.hudi.common.table.HoodieTableMetaClient;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 public class SDKConst {
     public static String catalog = "sdk_catalog";
 
-    public static String table_name = "sdk_table";
+    public static String cow_table_name = "cow_sdk_table";
+    public static String mor_table_name = "mor_sdk_table";
+    private static final String COW = HoodieTableType.COPY_ON_WRITE.name();
+    private static final String MOR = HoodieTableType.MERGE_ON_READ.name();
+    public static String location = "/Users/shaozengwei/projects/data/hudi";
 
-    public static String location = "/srv/datalake/hudi";
+
+    public static String recordKeyFields = "ids";
+
+    public static Schema hudi_schema;
+
+    static {
+        ArrayList<Schema.Field> fields = new ArrayList<>();
+        fields.add(new Schema.Field("id", Schema.create(Schema.Type.STRING), "desc", null));
+        fields.add(new Schema.Field("name", Schema.create(Schema.Type.STRING), "desc", null));
+        hudi_schema = Schema.createRecord(fields);
+    }
+    public static String getCowHudiTablePath() {
+        return Paths.get(location, cow_table_name).toString();
+    }
+
+    public static String getMorHudiTablePath() {
+        return Paths.get(location, mor_table_name).toString();
+    }
+
+
+    public static FileSystem createFileSystem(Configuration configuration) {
+        return FSUtils.getFs(location, configuration);
+    }
+
+    public static void initCopyOnWriteHudiTable(Configuration configuration) throws IOException {
+        FileSystem fileSystem = createFileSystem(configuration);
+
+        Path cowHudiTablePath = new Path(location);
+        if (!fileSystem.exists(cowHudiTablePath)) {
+            fileSystem.mkdirs(cowHudiTablePath);
+        }
+
+        HoodieTableMetaClient.withPropertyBuilder()
+                .setTableType(COW)
+                .setTableName(cow_table_name)
+                .setPayloadClassName(HoodieAvroPayload.class.getName())
+                .initTable(new Configuration(), getCowHudiTablePath());
+    }
 }
--- hudi/hudi-sdk/src/main/java/com/zw/sdk/write/HudiSDKWriteApplication.java
+++ hudi/hudi-sdk/src/main/java/com/zw/sdk/write/HudiSDKWriteApplication.java
@@ -1,4 +1,42 @@
 package com.zw.sdk.write;
 
+import com.zw.sdk.utils.SDKConst;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hudi.client.HoodieJavaWriteClient;
+import org.apache.hudi.client.common.HoodieJavaEngineContext;
+import org.apache.hudi.common.model.HoodieRecord;
+import org.apache.hudi.config.HoodieArchivalConfig;
+import org.apache.hudi.config.HoodieIndexConfig;
+import org.apache.hudi.config.HoodieWriteConfig;
+import org.apache.hudi.index.HoodieIndex;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class HudiSDKWriteApplication {
+    public static void main(String[] args) throws IOException {
+        SDKConst.initCopyOnWriteHudiTable(new Configuration());
+        HoodieWriteConfig config = HoodieWriteConfig.newBuilder()
+                .withPath(SDKConst.getCowHudiTablePath())
+                .withSchema(SDKConst.hudi_schema.toString(true))
+                .withParallelism(2, 2)
+                .forTable(SDKConst.cow_table_name)
+                .withIndexConfig(HoodieIndexConfig.newBuilder().withIndexType(HoodieIndex.IndexType.INMEMORY).build())
+                .withArchivalConfig(HoodieArchivalConfig.newBuilder().archiveCommitsWith(20, 30).build())
+                .build();
+        HoodieJavaWriteClient client = null;
+        try {
+
+            client = new HoodieJavaWriteClient(new HoodieJavaEngineContext(new Configuration()), config);
+
+            String startCommitTime = client.startCommit();
+
+            client.insert(new ArrayList<HoodieRecord>(), startCommitTime);
+        } finally {
+            if (client != null) {
+                client.close();
+            }
+        }
+    }
 }
